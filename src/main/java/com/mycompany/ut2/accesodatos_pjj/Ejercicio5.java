90083e2aafafcf282e7c11af25dbf6fb4298c586
DAM2P_PABLOJIMENEZ <2039470@alu.murciaeduca.es>
2026-09-25 16:31:02 +0200

    tarea hata la 6(6 sin hacer)

--- src/main/java/com/mycompany/ut2/accesodatos_pjj/Ejercicio5.java
+++ src/main/java/com/mycompany/ut2/accesodatos_pjj/Ejercicio5.java
@@ -1,13 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.ut2.accesodatos_pjj;
 
-/**
- *
- * @author DAM2P
- */
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Ejercicio5 {
-    
+
+    public static void main(String[] args) {
+
+        File fichero = new File("quijote.txt");
+
+        int lineas = 0;
+        int contadorQuijote = 0;
+
+        try (FileReader fr = new FileReader(fichero);
+             BufferedReader br = new BufferedReader(fr)) {
+
+            String linea;
+
+            while ((linea = br.readLine()) != null) {
+                lineas++;
+                String[] palabras = linea.split(" ");
+
+                for (int i = 0; i < palabras.length; i++) {
+                   // para comprobar si coincide con "Quijote"
+                    if (palabras[i].equals("Quijote")) {
+                        contadorQuijote++;
+                    }
+                }
+            }
+
+            System.out.println("Numero total de lineas: " + lineas);
+            System.out.println("La palabra Quijote aparece: " + contadorQuijote + " veces.");
+
+        } catch (IOException e) {
+            System.err.println("Error al leer el fichero: " + e.getMessage());
+        }
+    }
 }
---
+++ src/main/java/com/mycompany/ut2/accesodatos_pjj/Ejercicio6.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ut2.accesodatos_pjj;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author DAM2P
+ */
+public class Ejercicio6 {
+    Scanner scanner = new Scanner(System.in);
+    
+}
